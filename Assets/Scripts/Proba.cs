3839bef122f5a4af1ba3fdd858e4a73b0af80a9f
CwikUU <hujowe69@gmail.com>
2025-05-09 15:15:42 +0200

    State
    Wszystkie 4 state zrobione
    dziwnie przechodzi przez sciany i gracza
    po wejsciu w gracz dziwniw sie wystzreliwuje i drga

--- Assets/Scripts/Proba.cs
+++ Assets/Scripts/Proba.cs
@@ -12,26 +12,26 @@ public class Proba : MonoBehaviour
         Returning,
         Chasing,
     }
-
+    [Range(0, 10)]
     [SerializeField] private float speed; // Speed of the enemy
-    [SerializeField] private float roamRadius; // Radius within which the enemy roams
-    [SerializeField] private float roamTime; // Speed of the enemy while roaming
-    [SerializeField] private float waitTime; // Time to wait at each roaming position
+    [Range(0, 10)]
+    [SerializeField] private int waitTime; // Time to wait at each roaming position
+    [Range(0, 10)]
     [SerializeField] private float howFar;// How far the enemy can roam from the start position
+    [Range(0, 5)]
     [SerializeField] private float stopDistance;
-    public GameObject player; 
+    public GameObject player;
     private State state;
-    private EnemyPathfinding enemyPathfinding;
     private Vector2 startPosition;
     private Vector2 roamPosition;
     private Vector2 currentPosition;
     private Vector2 lastPlayerPosition;
     private Transform target;
     private bool inThis = false;
+    private float waitingTime = 0f;
 
     private void Awake()
     {
-        enemyPathfinding = GetComponent<EnemyPathfinding>();
         state = State.Roaming;
         startPosition = transform.position;
     }
@@ -40,17 +40,16 @@ public class Proba : MonoBehaviour
     {
         
         Debug.Log("Enemy AI started at position: " + startPosition);
-        
     }
 
     private void Update()
     {
         currentPosition = transform.position;
-        Debug.Log("target position: " + currentPosition + lastPlayerPosition+inThis+state+ Vector2.Distance(currentPosition, lastPlayerPosition));
+        Debug.Log("target position: " + currentPosition + roamPosition+inThis+state+ Vector2.Distance(currentPosition, roamPosition));
         EnemyRoutine();
     }
 
-    private void EnemyRoutine()
+    public void EnemyRoutine()
     {
         switch (state)
         {   
@@ -93,25 +92,44 @@ public class Proba : MonoBehaviour
         }
     }
 
-    
-
-    private Vector2 GetRoamingPosition()
-    {
-        return new Vector2(
-            Random.Range(-roamRadius, roamRadius),
-            Random.Range(-roamRadius, roamRadius)
-        );
-
-    }
-
     private void Waiting()
     {
-        Debug.Log("Waiting for " + waitTime + " seconds.");
+        Debug.Log("Waiting state: " );
+        if (waitingTime < waitTime)
+        {
+            waitingTime += Time.deltaTime;
+        }
+        else
+        {
+            waitingTime = 0f;
+            state = State.Roaming; // After waiting, go back to roaming
+            roamPosition = Vector2.zero;
+        }
     }
 
     private void Roaming()
     {
+        state = State.Roaming;
         Debug.Log("Roaming state: ");
+
+        if (roamPosition == Vector2.zero)
+        {
+            roamPosition = GetRoamingPosition();
+        }
+
+        if (roamPosition != Vector2.zero || Vector2.Distance(currentPosition, startPosition) == 0)
+        {
+            float step = speed * Time.deltaTime;
+            transform.position = Vector2.MoveTowards(currentPosition, roamPosition, step);
+
+            if (Vector2.Distance(currentPosition, roamPosition) < 0.1f)
+            {
+                Debug.Log("Enemy has reached the roaming position.");
+                state = State.Waiting;
+                
+            }
+
+        }
     }
 
     private void Chasing()
@@ -128,7 +146,7 @@ public class Proba : MonoBehaviour
             
             if (Vector2.Distance(currentPosition, lastPlayerPosition) == 0f )
             {
-                state = State.Roaming;
+                state = State.Returning;
                 Debug.Log("Enemy has stopped chasing the player." + state);
             }
         }
@@ -147,19 +165,27 @@ public class Proba : MonoBehaviour
         //Debug.Log("Distance to start position: " + Vector2.Distance(transform.position, startPosition));
         //Debug.Log("Speed: " + speed + state);
 
-        //if (Vector2.Distance(transform.position, startPosition) > 0.1f)
-        //{
-        //    Debug.Log("Moving towards start position");
-        //    float step = speed * Time.deltaTime;
-        //    transform.position = Vector2.MoveTowards(transform.position, startPosition, step);
-        //    Debug.Log("Speed: " + speed + end);
-        //}
-        //else
-        //{
-        //    Debug.Log("Enemy has returned to start position.");
-        //    end = true;
-        //    state = State.Roaming; // After returning, go back to roaming
-        //}
-      
+        if (Vector2.Distance(transform.position, roamPosition) > 0.1f)
+        {
+            Debug.Log("Moving towards start position");
+            float step = speed * Time.deltaTime;
+            transform.position = Vector2.MoveTowards(transform.position, roamPosition, step);
+        }
+        else
+        {
+            Debug.Log("Enemy has returned to start position.");
+            roamPosition = Vector2.zero;
+            state = State.Roaming; // After returning, go back to roaming
+        }
+
+    }
+
+    private Vector2 GetRoamingPosition()
+    {
+        return new Vector2(
+            Random.Range(startPosition.x-howFar, startPosition.x + howFar),
+            Random.Range(startPosition.y - howFar, startPosition.y + howFar)
+        );
+
     }
 }
